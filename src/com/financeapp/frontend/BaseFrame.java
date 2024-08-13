e6f2eb562736aaaeac3b41377c9b3e6bd0964d09
Kerciu <Kerciuuu@gmail.com>
2024-08-13 21:37:27 +0200

    moved login essentials to base frame

--- src/com/financeapp/frontend/BaseFrame.java
+++ src/com/financeapp/frontend/BaseFrame.java
@@ -1,8 +1,11 @@
 package com.financeapp.frontend;
 
 import javax.swing.*;
+import java.awt.*;
 
 public abstract class BaseFrame extends JFrame {
+    protected enum LabelType {CENTER_TEXT, USERNAME, PASSWORD};
+
     public BaseFrame(String title)
     {
         initializeComponents(title);
@@ -20,4 +23,70 @@ public abstract class BaseFrame extends JFrame {
     }
 
     protected abstract void addGuiComponents();
+
+    protected JLabel createLabel(String text, LabelType type)
+    {
+        JLabel label = new JLabel(text);
+        addLabelAttributes(label, type);
+        return label;
+    }
+
+    protected JTextField createTextField()
+    {
+        JTextField textField = new JTextField();
+        addTextFieldAttributes(textField);
+        return textField;
+    }
+
+    protected JPasswordField createPasswordField()
+    {
+        JPasswordField passwordField = new JPasswordField();
+        addPasswordFieldAttributes(passwordField);
+        return passwordField;
+    }
+
+    protected JButton createLoginButton()
+    {
+        JButton loginButton = new JButton("Login");
+        addLoginButtonAttributes(loginButton);
+        return loginButton;
+    }
+
+    protected void addLabelAttributes(JLabel label, LabelType type)
+    {
+        switch (type){
+            case CENTER_TEXT -> {
+                label.setBounds(0, 20, super.getWidth(), 40);
+                label.setFont(new Font("Dialog", Font.BOLD, 32));
+                label.setHorizontalAlignment(SwingConstants.CENTER);
+            }
+            case USERNAME -> {
+                label.setBounds(20, 120, super.getWidth() - 30, 24);
+                label.setFont(new Font("Dialog", Font.PLAIN, 20));
+            }
+            case PASSWORD -> {
+                label.setBounds(20, 280, super.getWidth() - 30, 24);
+                label.setFont(new Font("Dialog", Font.PLAIN, 20));
+            }
+        }
+    }
+
+    protected void addTextFieldAttributes(JTextField textField)
+    {
+        textField.setBounds(20, 160, super.getWidth() - 50, 40);
+        textField.setFont(new Font("Dialog", Font.PLAIN, 28));
+    }
+
+    protected void addPasswordFieldAttributes(JPasswordField passwordField)
+    {
+        passwordField.setBounds(20, 320, super.getWidth() - 50, 40);
+        passwordField.setFont(new Font("Dialog", Font.PLAIN, 28));
+    }
+
+    protected void addLoginButtonAttributes(JButton loginButton)
+    {
+        loginButton.setBounds(20, 460, super.getWidth() - 50, 40);
+        loginButton.setFont(new Font("Dialog", Font.BOLD, 20));
+        loginButton.setHorizontalAlignment(SwingConstants.CENTER);
+    }
 }
--- src/com/financeapp/frontend/LoginFrame.java
+++ src/com/financeapp/frontend/LoginFrame.java
@@ -4,8 +4,6 @@ import javax.swing.*;
 import java.awt.*;
 
 public class LoginFrame extends BaseFrame{
-    private enum LabelType {CENTER_TEXT, USERNAME, PASSWORD};
-
     public LoginFrame()
     {
         super("Finance App - Sign In");
@@ -22,34 +20,6 @@ public class LoginFrame extends BaseFrame{
         add(createRegisterLabel());
     }
 
-    private JLabel createLabel(String text, LabelType type)
-    {
-        JLabel label = new JLabel(text);
-        addLabelAttributes(label, type);
-        return label;
-    }
-
-    private JTextField createTextField()
-    {
-        JTextField textField = new JTextField();
-        addTextFieldAttributes(textField);
-        return textField;
-    }
-
-    private JPasswordField createPasswordField()
-    {
-        JPasswordField passwordField = new JPasswordField();
-        addPasswordFieldAttributes(passwordField);
-        return passwordField;
-    }
-
-    private JButton createLoginButton()
-    {
-        JButton loginButton = new JButton("Login");
-        addLoginButtonAttributes(loginButton);
-        return loginButton;
-    }
-
     private JLabel createRegisterLabel()
     {
         String text = "No account yet? Click here to sign up!";
@@ -58,45 +28,7 @@ public class LoginFrame extends BaseFrame{
         return registerLabel;
     }
 
-    private void addLabelAttributes(JLabel label, LabelType type)
-    {
-        switch (type){
-            case CENTER_TEXT -> {
-                label.setBounds(0, 20, super.getWidth(), 40);
-                label.setFont(new Font("Dialog", Font.BOLD, 32));
-                label.setHorizontalAlignment(SwingConstants.CENTER);
-            }
-            case USERNAME -> {
-                label.setBounds(20, 120, super.getWidth() - 30, 24);
-                label.setFont(new Font("Dialog", Font.PLAIN, 20));
-            }
-            case PASSWORD -> {
-                label.setBounds(20, 280, super.getWidth() - 30, 24);
-                label.setFont(new Font("Dialog", Font.PLAIN, 20));
-            }
-        }
-    }
-
-    private void addTextFieldAttributes(JTextField textField)
-    {
-        textField.setBounds(20, 160, super.getWidth() - 50, 40);
-        textField.setFont(new Font("Dialog", Font.PLAIN, 28));
-    }
-
-    private void addPasswordFieldAttributes(JPasswordField passwordField)
-    {
-        passwordField.setBounds(20, 320, super.getWidth() - 50, 40);
-        passwordField.setFont(new Font("Dialog", Font.PLAIN, 28));
-    }
-
-    private void addLoginButtonAttributes(JButton loginButton)
-    {
-        loginButton.setBounds(20, 460, super.getWidth() - 50, 40);
-        loginButton.setFont(new Font("Dialog", Font.BOLD, 20));
-        loginButton.setHorizontalAlignment(SwingConstants.CENTER);
-    }
-
-    private void addRegisterLabelAttributes(JLabel registerLabel)
+    private void addRegisterLabelAttributes (JLabel registerLabel)
     {
         registerLabel.setBounds(0, 510, super.getWidth() - 10, 30);
         registerLabel.setFont(new Font("Dialog", Font.PLAIN, 20));
