a08a67b327745a59d8063c363b030fac37169401
Kerciu <Kerciuuu@gmail.com>
2024-08-15 12:01:38 +0200

    added login gui disposing after succesfully signing in

--- src/com/financeapp/frontend/authentication/LoginFrame.java
+++ src/com/financeapp/frontend/authentication/LoginFrame.java
@@ -1,10 +1,18 @@
 package com.financeapp.frontend.authentication;
 
+import com.financeapp.backend.data.User;
+import com.financeapp.backend.db.MySQLConnector;
+import com.financeapp.frontend.app.MainFrame;
 import com.financeapp.frontend.components.UIComponentFactory;
+import com.mysql.cj.log.Log;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class LoginFrame extends AuthenticationBaseFrame{
+    private JTextField usernameTextField;
+    private JPasswordField passwordField;
     public LoginFrame()
     {
         super("Finance App - Sign In");
@@ -15,14 +23,55 @@ public class LoginFrame extends AuthenticationBaseFrame{
         add(createLabel("Finance Manager", LabelType.CENTER_TEXT));
         add(createLabel("Username", LabelType.USERNAME));
         add(createLabel("Password", LabelType.PASSWORD));
-        add(createTextField());
-        add(createPasswordField());
+
+        usernameTextField = createTextField();
+        add(usernameTextField);
+        passwordField = createPasswordField();
+        add(passwordField);
+
         add(createLoginButton());
         add(createRegisterLabel());
     }
 
     private JButton createLoginButton() {
-        return UIComponentFactory.createButton("Login", 20, 460, super.getWidth() - 50, 40, 20);
+        JButton loginButton = UIComponentFactory.createButton("Login", 20, 460, super.getWidth() - 50, 40, 20);
+        loginButton.addActionListener(
+                createLoginButtonActionListener()
+        );
+
+        return loginButton;
+    }
+
+    private ActionListener createLoginButtonActionListener()
+    {
+        return new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String username = usernameTextField.getText();
+                String password = String.valueOf(passwordField.getPassword());
+
+                User user = MySQLConnector.validateLogin(username, password);
+                if (user != null) {
+                    disposeLoginFrame(user);
+                } else {
+                    showFailureWindow();
+                }
+            }
+        };
+    }
+
+    private void disposeLoginFrame(User user)
+    {
+        LoginFrame.this.dispose();
+        MainFrame mainFrame = new MainFrame(user);
+        mainFrame.setVisible(true);
+
+        JOptionPane.showMessageDialog(mainFrame, "Signed In Succesfully!");
+    }
+
+    private void showFailureWindow()
+    {
+        JOptionPane.showMessageDialog(LoginFrame.this, "Username or Password does not match!");
     }
 
     private JLabel createRegisterLabel() {
