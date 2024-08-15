0f1a9ee472786b6313101bc236bbd9f6790b6b72
Kerciu <Kerciuuu@gmail.com>
2024-08-15 11:41:05 +0200

    added password utils class hashing the password and validating it

---
+++ src/com/financeapp/backend/db/PasswordUtils.java
@@ -0,0 +1,15 @@
+package com.financeapp.backend.db;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtils {
+    public static String hashPassword(String password)
+    {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+
+    public static boolean checkIfPasswordMatches(String password, String hashedPassword)
+    {
+        return BCrypt.checkpw(password, hashedPassword);
+    }
+}
