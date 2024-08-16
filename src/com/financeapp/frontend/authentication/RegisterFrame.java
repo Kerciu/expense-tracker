b9b4d2ace626ac1bcb4213e32297c61dd5fa9cc9
Kerciu <Kerciuuu@gmail.com>
2024-08-16 10:13:06 +0200

    added clickable label in signing up frame

--- src/com/financeapp/frontend/authentication/RegisterFrame.java
+++ src/com/financeapp/frontend/authentication/RegisterFrame.java
@@ -3,7 +3,8 @@ package com.financeapp.frontend.authentication;
 import com.financeapp.frontend.components.UIComponentFactory;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class RegisterFrame extends AuthenticationBaseFrame {
 
@@ -23,6 +24,28 @@ public class RegisterFrame extends AuthenticationBaseFrame {
         add(UIComponentFactory.createPasswordField(20, 360, getWidth() - 50, 40, 28));
 
         add(UIComponentFactory.createButton("Register", 20, 460, getWidth() - 50, 40, 20));
-        add(UIComponentFactory.createLabel("Already have an account? Click here to sign in!", 0, 510, getWidth() - 10, 30, 16, SwingConstants.CENTER));
+        add(createLoginLabel());
     }
+
+    private JLabel createLoginLabel()
+    {
+        JLabel label = UIComponentFactory.createLabel("Already have an account? Click here to sign in!", 0, 510, getWidth() - 10, 30, 16, SwingConstants.CENTER);
+        label.addMouseListener(
+                createMouseListener()
+        );
+
+        return label;
+    }
+
+    private MouseAdapter createMouseListener()
+    {
+        return new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                RegisterFrame.this.dispose();
+                new LoginFrame().setVisible(true);
+            }
+        };
+    }
+
 }
