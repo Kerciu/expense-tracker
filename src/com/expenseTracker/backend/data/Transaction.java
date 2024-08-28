80b98e8bb719a9737e90776fcf8a8fb2e8c6f389
Kerciu <Kerciuuu@gmail.com>
2024-08-28 10:24:27 +0200

    added date to the transaction class

--- src/com/expenseTracker/backend/data/Transaction.java
+++ src/com/expenseTracker/backend/data/Transaction.java
@@ -2,20 +2,37 @@ package com.expenseTracker.backend.data;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class Transaction {
     private final int id;
     private final int userId;
     private BigDecimal amount;
+    private LocalDate date;
     private String type;
     private String category;
     private String description;
+
+    private static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     public Transaction(int id, int userId, BigDecimal amount, String type, String category, String description)
     {
         this.id = id;
         this.userId = userId;
         this.amount = amount;
+        this.date = LocalDate.now();
+        this.type = type;
+        this.category = category;
+        this.description = description;
+    }
+
+    public Transaction(int id, int userId, BigDecimal amount, LocalDate date, String type, String category, String description)
+    {
+        this.id = id;
+        this.userId = userId;
+        this.amount = amount;
+        this.date = date;
         this.type = type;
         this.category = category;
         this.description = description;
@@ -33,6 +50,16 @@ public class Transaction {
         return amount;
     }
 
+    public LocalDate getDate()
+    {
+        return date;
+    }
+
+    public String getFormattedDate()
+    {
+        return date.format(DATE_FORMATTER);
+    }
+
     public String getType() {
         return type;
     }
@@ -48,6 +75,11 @@ public class Transaction {
     public void setAmount(BigDecimal amount)
     {
         this.amount = amount.setScale(2, RoundingMode.FLOOR);
+    }
+
+    public void setDate(LocalDate date)
+    {
+        this.date = date;
     }
 
     public void setType(String type)
