0c3fb1019d268079d5283f95b92ef7da534b5847
Kerciu <Kerciuuu@gmail.com>
2024-08-30 10:45:40 +0200

    linked iText library to the project

--- src/com/expenseTracker/backend/writers/PDFExporter.java
+++ src/com/expenseTracker/backend/writers/PDFExporter.java
@@ -1,4 +1,31 @@
 package com.expenseTracker.backend.writers;
 
-public class PDFExporter {
+
+import com.expenseTracker.backend.data.User;
+import com.itextpdf.kernel.pdf.PdfDocument;
+import com.itextpdf.kernel.pdf.PdfWriter;
+import com.itextpdf.layout.Document;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class PDFExporter extends FileExporter {
+    public PDFExporter(String filePath, User user) {
+        super(filePath, user);
+    }
+
+    @Override
+    public void exportFile() {
+        if (transactionList == null || transactionList.isEmpty()) return;
+
+        try {
+            PdfWriter pdfWriter = new PdfWriter(filePath);
+            PdfDocument pdfDocument = new PdfDocument(pdfWriter);
+            Document document = new Document(pdfDocument);
+
+        }
+        catch(IOException e) {
+            throw new RuntimeException();
+        }
+    }
 }
--- src/com/expenseTracker/backend/writers/XLSXExporter.java
+++ src/com/expenseTracker/backend/writers/XLSXExporter.java
@@ -1,4 +1,14 @@
 package com.expenseTracker.backend.writers;
 
-public class XLSXExporter {
+import com.expenseTracker.backend.data.User;
+
+public class XLSXExporter extends FileExporter{
+    public XLSXExporter(String filePath, User user) {
+        super(filePath, user);
+    }
+
+    @Override
+    public void exportFile() {
+
+    }
 }
